2f88929844d6d8423af35fc68523ae7c70e49713
User <User@WIN-32NOSR5DPIA>
2025-04-10 14:13:10 +0500

    Последнее 1 22 05

---
+++ AppTitlesAnime/FormListTypes.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace AppTitlesAnime {
+	partial class FormListTypes {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			flowLayoutPanel1 = new FlowLayoutPanel();
+			SuspendLayout();
+			// 
+			// flowLayoutPanel1
+			// 
+			flowLayoutPanel1.Dock = DockStyle.Top;
+			flowLayoutPanel1.ForeColor = SystemColors.ActiveCaption;
+			flowLayoutPanel1.Location = new Point(0, 0);
+			flowLayoutPanel1.Name = "flowLayoutPanel1";
+			flowLayoutPanel1.Size = new Size(799, 100);
+			flowLayoutPanel1.TabIndex = 0;
+			// 
+			// FormListTypes
+			// 
+			AutoScaleDimensions = new SizeF(11F, 25F);
+			AutoScaleMode = AutoScaleMode.Font;
+			BackColor = Color.White;
+			ClientSize = new Size(799, 531);
+			Controls.Add(flowLayoutPanel1);
+			Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+			Margin = new Padding(5, 5, 5, 5);
+			Name = "FormListTypes";
+			Text = "FormListTypes";
+			ResumeLayout(false);
+		}
+
+		#endregion
+
+		private FlowLayoutPanel flowLayoutPanel1;
+	}
+}
